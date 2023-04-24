ace1ee66be775908bcdbb0b08886f9758550a9f5
Akash Boora <akash.boora@zemosolabs.com>
2023-04-24 12:15:48 +0530

    feat: added basic directives

--- my-first-angular-app/src/app/servers/servers.component.ts
+++ my-first-angular-app/src/app/servers/servers.component.ts
@@ -12,13 +12,18 @@ export class ServersComponent {
   allowNewServer = false;
   serverCreationStatus = 'No server was created';
   serverName = 'TestServer';
+  serverCreated = false;
+  servers = ['TestServer 1', 'TestServer 2'];
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
     }, 2000);
   }
   onCreateServer() {
-    this.serverCreationStatus = 'Server was created with name '+ this.serverName;
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+    this.serverCreationStatus =
+      'Server was created with name ' + this.serverName;
   }
   onUpdateServerName(event: Event) {
     this.serverName = (<HTMLInputElement>event.target).value;
