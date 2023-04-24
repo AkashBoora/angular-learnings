2641817976b64114a93364278a8ac783f8d5fdfd
Akash Boora <akash.boora@zemosolabs.com>
2023-04-24 10:23:58 +0530

    feat: added data binding basics

--- my-first-angular-app/src/app/servers/servers.component.ts
+++ my-first-angular-app/src/app/servers/servers.component.ts
@@ -6,8 +6,21 @@ import { Component } from '@angular/core';
   // selector: '.app-servers',
   // template: '<p>In Servers Component Inline</p><app-server></app-server>',
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent {
-
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created';
+  serverName = 'TestServer';
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created with name '+ this.serverName;
+  }
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }
