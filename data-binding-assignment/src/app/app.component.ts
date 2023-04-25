4587559a9719c87506bcf69e839316a0a766310a
Akash Boora <akash.boora@zemosolabs.com>
2023-04-25 19:23:11 +0530

    feat: added solution for data-binding assignment

--- data-binding-assignment/src/app/app.component.ts
+++ data-binding-assignment/src/app/app.component.ts
@@ -3,7 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  oddNumbers: number[] = [];
+  evenNumbers: number[] = [];
+
+  onIntervalFired(firedNumber: number) {
+    if(firedNumber%2 ===1){
+      this.oddNumbers.push(firedNumber);
+    }else {
+      this.evenNumbers.push(firedNumber);
+    }
+  }
 }
