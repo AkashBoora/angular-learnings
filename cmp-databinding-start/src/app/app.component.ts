ec30904281370c29a5d159aea98b9aa442380a80
Akash Boora <akash.boora@zemosolabs.com>
2023-04-25 14:09:31 +0530

    component life cycle

--- cmp-databinding-start/src/app/app.component.ts
+++ cmp-databinding-start/src/app/app.component.ts
@@ -27,4 +27,12 @@ export class AppComponent {
       content: bluePrintDat.bluePrintContent,
     });
   }
+
+  onChangeFirst() {
+    this.serverElements[0].name = 'Changed!';
+  }
+
+  onDestroyFirst() {
+    this.serverElements.splice(0, 1);
+  }
 }
--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,15 +1,73 @@
-import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  ContentChild,
+  DoCheck,
+  ElementRef,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges,
+  ViewChild,
+  ViewEncapsulation,
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css'],
-  encapsulation: ViewEncapsulation.Emulated
+  encapsulation: ViewEncapsulation.Emulated,
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent
+  implements
+    OnInit,
+    OnChanges,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked,
+    OnDestroy
+{
   @Input('srvElement') element: { type: string; name: string; content: string };
+  @Input() name: string;
+  @ViewChild('heading', { static: true }) header: ElementRef;
+  @ContentChild('contentParagraph', { static: true }) contentChild: ElementRef;
 
-  constructor() {}
-
-  ngOnInit(): void {}
+  constructor() {
+    console.log('constructor called');
+  }
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnChanges called');
+    console.log(changes);
+  }
+  ngOnInit(): void {
+    console.log("header text content " +this.header.nativeElement.textContent);
+    console.log("paragraph text content " +this.contentChild.nativeElement.textContent);
+    console.log('ngOnInit called');
+  }
+  ngDoCheck(): void {
+    console.log('ngDoCheck');
+  }
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit called');
+  }
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked called!');
+  }
+  ngAfterViewInit(): void {
+    console.log("header text content " +this.header.nativeElement.textContent);
+    console.log("paragraph text content " +this.contentChild.nativeElement.textContent);
+    console.log('ngAfterViewInit called!');
+  }
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked called!');
+  }
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy Called');
+  }
 }
