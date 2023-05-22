e270af892dee178c4c60f3d4ce9f93858d097d8c
Akash Boora <akash.boora@zemosolabs.com>
2023-05-22 21:19:52 +0530

    changed logic using observables

--- app-project/src/app/recipes/recipe.service.ts
+++ app-project/src/app/recipes/recipe.service.ts
@@ -1,11 +1,10 @@
-import { EventEmitter, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Recipe } from './recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shoppipng-list.service';
 
 @Injectable({ providedIn: 'root' })
 export class RecipeService {
-  recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
     new Recipe(
       'Tasty Schnitzel',
