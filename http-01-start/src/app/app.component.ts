b0ee7ddf083246a22e6b87d50c98053f7353ac7a
Akash Boora <akash.boora@zemosolabs.com>
2023-05-24 20:19:59 +0530

    handling error way 1

--- http-01-start/src/app/app.component.ts
+++ http-01-start/src/app/app.component.ts
@@ -11,6 +11,7 @@ import { PostService } from "./post.service";
 export class AppComponent implements OnInit {
   loadedPosts: Post[] = [];
   isFetching = false;
+  error = null;
 
   constructor(private http: HttpClient, private postService: PostService) {}
 
@@ -19,6 +20,8 @@ export class AppComponent implements OnInit {
     this.postService.fetchPosts().subscribe(posts => {
       this.isFetching=false;
       this.loadedPosts = posts;
+    }, error=> {
+      this.error = error.message;
     });
   }
 
@@ -33,6 +36,8 @@ export class AppComponent implements OnInit {
     this.postService.fetchPosts().subscribe(posts => {
       this.isFetching=false;
       this.loadedPosts = posts;
+    }, error=> {
+      this.error = error.message;
     });
   }
 
