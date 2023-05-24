97472b27af9f6d02d754d999c4ce37098f6d2723
Akash Boora <akash.boora@zemosolabs.com>
2023-05-24 20:24:12 +0530

    error handling 2 way

--- http-01-start/src/app/app.component.ts
+++ http-01-start/src/app/app.component.ts
@@ -1,21 +1,26 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Post } from "./post.model";
 import { PostService } from "./post.service";
+import { Subscription } from "rxjs";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"],
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   loadedPosts: Post[] = [];
   isFetching = false;
   error = null;
+  private errorSub : Subscription;
 
   constructor(private http: HttpClient, private postService: PostService) {}
 
   ngOnInit() {
+    this.errorSub =  this.postService.error.subscribe( errorMessage => {
+      this.error = errorMessage;
+    })
     this.isFetching= true;
     this.postService.fetchPosts().subscribe(posts => {
       this.isFetching=false;
@@ -46,6 +51,10 @@ export class AppComponent implements OnInit {
     this.postService.deletePosts().subscribe(() => {
       this.loadedPosts = [];
     });
+  }
+
+  ngOnDestroy(): void {
+      this.errorSub.unsubscribe();
   }
 
 }
--- http-01-start/src/app/post.service.ts
+++ http-01-start/src/app/post.service.ts
@@ -2,9 +2,11 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Post } from "./post.model";
 import { map } from "rxjs/operators";
+import { Subject } from "rxjs";
 
 @Injectable({providedIn:'root'})
 export class PostService {
+  error = new Subject<string>();
   constructor(private http: HttpClient){}
   createAndStorePost(postData: Post){
     this.http
@@ -14,7 +16,7 @@ export class PostService {
     )
     .subscribe((responseData) => {
       console.log(responseData);
-    });
+    }, error => this.error.next(error.message));
   }
 
   fetchPosts() {
