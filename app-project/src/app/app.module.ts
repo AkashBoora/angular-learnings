a196d432e20be458541b1d82b3a8dec86ead9639
Akash Boora <akash.boora@zemosolabs.com>
2023-05-25 13:46:41 +0530

    added http service for recipes

--- app-project/src/app/app.module.ts
+++ app-project/src/app/app.module.ts
@@ -13,7 +13,7 @@ import { DropdownDirective } from './shared/dropdown.directive';
 import { AppRoutingModule } from './app-routing.module';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
-
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent,
@@ -32,7 +32,8 @@ import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component
     BrowserModule,
     FormsModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- app-project/src/app/recipes/recipe.service.ts
+++ app-project/src/app/recipes/recipe.service.ts
@@ -8,20 +8,22 @@ import { Subject } from 'rxjs';
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Tasty Schnitzel',
-      'A super-tasty Schnitzel - just awesome',
-      'https://bakeitwithlove.com/wp-content/uploads/2022/09/chicken_schnitzel_sq.jpg',
-      [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
-    ),
-    new Recipe(
-      'Big Fat Burger',
-      'What else you need to say?',
-      'https://img.freepik.com/premium-photo/delicious-hamburger-with-tasty-ingredients_184700-4.jpg?w=826',
-      [new Ingredient('Meat', 1), new Ingredient('Bread', 2)]
-    ),
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'Tasty Schnitzel',
+  //     'A super-tasty Schnitzel - just awesome',
+  //     'https://bakeitwithlove.com/wp-content/uploads/2022/09/chicken_schnitzel_sq.jpg',
+  //     [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
+  //   ),
+  //   new Recipe(
+  //     'Big Fat Burger',
+  //     'What else you need to say?',
+  //     'https://img.freepik.com/premium-photo/delicious-hamburger-with-tasty-ingredients_184700-4.jpg?w=826',
+  //     [new Ingredient('Meat', 1), new Ingredient('Bread', 2)]
+  //   ),
+  // ];
+
+  private recipes: Recipe[] = [];
 
   constructor(private shoppingListService: ShoppingListService) {}
   getRecipes() {
@@ -50,5 +52,10 @@ export class RecipeService {
   deleteRecipe(index: number){
     this.recipes.splice(index,1);
     this.recipesChanged.next(this.recipes.slice());
+  }
+
+  setRecipes(recipes: Recipe[]){
+    this.recipes = recipes;
+    this.recipesChanged.next(recipes.slice());
   }
 }
