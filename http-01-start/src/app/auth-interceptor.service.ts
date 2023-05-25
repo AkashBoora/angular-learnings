45f857e6d4755747ff66c476ac682bb6c67a6032
Akash Boora <akash.boora@zemosolabs.com>
2023-05-25 09:46:18 +0530

    response interceptor

--- http-01-start/src/app/auth-interceptor.service.ts
+++ http-01-start/src/app/auth-interceptor.service.ts
@@ -1,14 +1,36 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import {
+  HttpEvent,
+  HttpEventType,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
+import { tap } from "rxjs/operators";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
-export class AuthInterceptorService implements HttpInterceptor{
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    console.log('Request on its way');
-    return next.handle(req);
+export class AuthInterceptorService implements HttpInterceptor {
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    const modifiedRequest = req.clone({
+      headers: req.headers.append("Auth", "token"),
+    });
+    console.log("Request on its way");
+    return next.handle(modifiedRequest).pipe(
+      tap((event) => {
+        if (event.type == HttpEventType.Sent) {
+          console.log(event.type);
+        }
+        if (event.type == HttpEventType.Response) {
+          console.log('Response Arrived');
+          console.log(event.body);
+        }
+      })
+    );
   }
-
 }
