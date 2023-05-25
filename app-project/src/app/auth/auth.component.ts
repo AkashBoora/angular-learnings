52846b37098a6f2e25672881e279d67202b2f3ea
Akash Boora <akash.boora@zemosolabs.com>
2023-05-25 19:33:09 +0530

    added sign up functionality

--- app-project/src/app/auth/auth.component.ts
+++ app-project/src/app/auth/auth.component.ts
@@ -1,20 +1,38 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.css']
+  styleUrls: ['./auth.component.css'],
 })
 export class AuthComponent {
   isLoginMode = true;
+
+  constructor(private authService: AuthService) {}
 
   onSwitchMode() {
     this.isLoginMode = !this.isLoginMode;
   }
 
-  onSubmit(form:NgForm){
-    console.log(form.value);
+  onSubmit(form: NgForm) {
+    if (!form.valid) {
+      return;
+    }
+    if (this.isLoginMode) {
+    } else {
+      const email = form.value.email;
+      const password = form.value.password;
+      this.authService.signUp(email, password).subscribe(
+        (resData) => {
+          console.log(resData);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    }
     form.reset();
   }
 }
