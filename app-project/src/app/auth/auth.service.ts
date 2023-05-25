bad0b9210585ea43f11f4dcc16041ffeae22f281
Akash Boora <akash.boora@zemosolabs.com>
2023-05-25 20:53:07 +0530

    login functionality

--- app-project/src/app/auth/auth.service.ts
+++ app-project/src/app/auth/auth.service.ts
@@ -1,14 +1,15 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { catchError, throwError } from 'rxjs';
 
-interface AuthResponseData {
+export interface AuthResponseData {
   kind: string;
   idToken: string;
   email: string;
   refreshToken: string;
   expiresIn: string;
   localId: string;
+  registered?: boolean;
 }
 
 @Injectable({ providedIn: 'root' })
@@ -25,26 +26,39 @@ export class AuthService {
           returnSecureToken: true,
         }
       )
-      .pipe(
-        catchError((errorRes) => {
-          let errorMessage = 'An unknown error occurred';
-          if (!errorRes.error && !!errorRes.error.error) {
-            return throwError(errorMessage);
-          }
-          switch (errorRes.error.error.message) {
-            case 'EMAIL_EXISTS':
-              errorMessage = 'This Email already exist';
-              break;
-            case 'OPERATION_NOT_ALLOWED':
-              errorMessage = 'Wrong Email or password exist';
-              break;
-            case 'TOO_MANY_ATTEMPTS_TRY_LATER':
-              errorMessage =
-                'Too many requests, Please try again after sometime.';
-              break;
-          }
-          return throwError(errorMessage);
-        })
-      );
+      .pipe(catchError(this.handleError));
+  }
+
+  login(email: string, password: string) {
+    return this.http
+      .post<AuthResponseData>(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDfUGc3N0nbEbf3ZxJM-ftzgMPeY0lyyS8',
+        {
+          email: email,
+          password: password,
+          returnSecureToken: true,
+        }
+      )
+      .pipe(catchError(this.handleError));
+  }
+
+  private handleError(errorRes: HttpErrorResponse) {
+    let errorMessage = 'An unknown error occurred';
+    if (!errorRes.error && !!errorRes.error.error) {
+      return throwError(errorMessage);
+    }
+    switch (errorRes.error.error.message) {
+      case 'EMAIL_EXISTS':
+        errorMessage = 'This Email already exist';
+        break;
+      case 'OPERATION_NOT_ALLOWED':
+        errorMessage = 'Wrong Email or password exist';
+        break;
+      case 'TOO_MANY_ATTEMPTS_TRY_LATER':
+        errorMessage = 'Too many requests, Please try again after sometime.';
+        break;
+      default: errorMessage = errorRes.error.error.message; break;
+    }
+    return throwError(errorMessage);
   }
 }
