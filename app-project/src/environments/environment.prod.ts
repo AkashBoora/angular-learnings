8ed6844639a0d80849fa8c6b2d0ac885fc2ff529
Akash Boora <akash.boora@zemosolabs.com>
2023-05-29 18:54:40 +0530

    deployed into firebase

---
+++ app-project/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  fireBaseAPIKey:  'AIzaSyDfUGc3N0nbEbf3ZxJM-ftzgMPeY0lyyS8'
+};
---
+++ app-project/src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  fireBaseAPIKey:  'AIzaSyDfUGc3N0nbEbf3ZxJM-ftzgMPeY0lyyS8'
+};
