d7ac2049e4d9e3c9d6fe1cff55364a9ec1624619
Akash Boora <akash.boora@zemosolabs.com>
2023-05-29 16:00:11 +0530

    added core module

--- app-project/src/app/app.module.ts
+++ app-project/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { AuthInterceptorService } from './auth/auth-interceptor.service';
 import { RecipesModule } from './recipes/recipes.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core.module';
 @NgModule({
   declarations: [
     AppComponent,
@@ -24,9 +25,9 @@ import { SharedModule } from './shared/shared.module';
     HttpClientModule,
     RecipesModule,
     ShoppingListModule,
-    SharedModule
+    SharedModule,
+    CoreModule
   ],
-  providers: [{provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
