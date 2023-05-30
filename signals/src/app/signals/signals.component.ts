5f8d71c7a3ee0aa60ad35598e3c82996a90ba0dc
Akash Boora <akash.boora@zemosolabs.com>
2023-05-30 20:05:09 +0530

    different update methods

--- signals/src/app/signals/signals.component.ts
+++ signals/src/app/signals/signals.component.ts
@@ -13,6 +13,11 @@ export class SignalComponent {
 
   increment() {
     this.counter.update(oldValue => oldValue+1);
+
+    //update, set, mutate
+    // update takes functions where we can update value by using old value
+    // set directly sets to value
+    // mutate is same as update but it creates new value rather then updating previous value
     this.actions.push('INCREMENT');
   }
 
